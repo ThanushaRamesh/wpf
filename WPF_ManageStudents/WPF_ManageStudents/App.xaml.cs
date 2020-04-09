3f5378ac2d8c5593fb6b249b2eea24c8956999de
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2020-04-09 12:56:41 +0200

    Initial Commit 07 April

---
+++ WPF_ManageStudents/WPF_ManageStudents/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_ManageStudents
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static List<Student> _students;
+        public App() { }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //get data from storage
+            if(_students == null)
+            {
+                // _students = new List<Student>();
+                _students = GenerateStudents(30);
+            }
+        }
+
+        private List<Student> GenerateStudents(int cnt)
+        {
+            var lst = new List<Student>();
+            for (int i = 0; i < cnt; i++)
+            {
+                lst.Add(new Student
+                {
+                    firstName =
+                    "fname" + i,
+                    lastName = "lname" + i
+                });
+            }
+            return lst;
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPF_ManageStudents/WPF_ManageStudents/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace WPF_ManageStudents
+{
+    public class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
