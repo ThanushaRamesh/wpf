8ec69c6fc45b9d12f51065a91b09a8bc95693ed0
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2020-04-19 21:02:38 +0200

    April 16 2020

---
+++ WPF_ManageStudents_16_April/WPF_ManageStudents/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace WPF_ManageStudents
+{
+    public class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public string matNumber { get; set; }
+
+        public string gender { get; set; } //m= male, f= female
+
+        [System.Xml.Serialization.XmlIgnore]
+        public string name { get { return $"{ firstName} { lastName}"; } }
+        public string MyProperty { get; set; }
+
+    }
+}
---
+++ WPF_ManageStudents_16_April/WPF_ManageStudents/W_Test.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_ManageStudents
+{
+    /// <summary>
+    /// Interaction logic for W_Test.xaml
+    /// </summary>
+    public partial class W_Test : Window
+    {
+        public W_Test()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Owner.Visibility = Visibility.Visible;
+        }
+    }
+}
