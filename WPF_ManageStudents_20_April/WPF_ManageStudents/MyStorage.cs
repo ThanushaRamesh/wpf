82bd92a7fa5f129d06e7ca57ca0bfc7a096bb9ea
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2020-04-20 15:30:01 +0200

    April 20 2020

---
+++ WPF_ManageStudents_20_April/WPF_ManageStudents/MyStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WPF_ManageStudents
+{
+    internal class MyStorage
+    {
+        internal static void WriteXml<T>(T data, string file)
+        {
+            XmlSerializer sr = new XmlSerializer(typeof(T));
+
+            FileStream stream;
+
+            stream = new FileStream(file, FileMode.Create);
+
+            sr.Serialize(stream, data);
+            stream.Close(); // MAking sure that file is closed and doesnt cause any issue while deleting
+        }
+    }
+}
