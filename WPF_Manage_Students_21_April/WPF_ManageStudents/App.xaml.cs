1e6809abce1cd3010be8eb3bca44e71379f177f2
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2020-04-22 12:15:27 +0200

    April 21

---
+++ WPF_Manage_Students_21_April/WPF_ManageStudents/App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_ManageStudents
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ObservableCollection<Student> _students;
+        Random rnd = new Random(Guid.NewGuid().GetHashCode());
+        public static List<string> _genders = new List<string> { "m", "f", "d" };
+
+        public App() { }
+
+        private void Application_Startup(object sender, StartupEventArgs e)                                                                 
+        {
+
+            //get data from storage
+            _students = MyStorage.ReadXML<ObservableCollection<Student>>("students.xml");
+
+            if(_students == null)
+            {
+                _students = new ObservableCollection<Student>();
+                //_students = GenerateStudents(50);
+            }
+        }
+
+        private ObservableCollection<Student> GenerateStudents(int cnt)
+        {
+            List<string> fNames = new List<string> {"Bhavana","Shivani","Thanusha","Priya","Pallavi","Divya","Ram","Bheem","Nakul","Sahadeva","Lakshman" };
+            List<string> lNames = new List<string> { "Blname", "Slname", "Tlname","Plname", "Dlname", "Rlname","Bhlname","Nlname", "Shname", "Llnmae"  };
+            var lst = new ObservableCollection<Student>();
+            for (int i = 0; i < cnt; i++)
+            {
+                int fNo = rnd.Next(fNames.Count);
+                int lNo = rnd.Next(lNames.Count);
+                var gndr = fNo > 5 ? "m" : "f";
+
+                lst.Add(new Student
+                {
+                    firstName = fNames[fNo], lastName = lNames[lNo],gender = gndr,
+                    matNumber = Math.Abs(Guid.NewGuid().GetHashCode()).ToString()
+                });
+            }
+            return lst;
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            //Mystorage- Class..WriteXML method, <>type
+            MyStorage.WriteXml<ObservableCollection<Student>>(_students, "students.xml");
+
+        }
+    }
+}
---
+++ WPF_Manage_Students_21_April/WPF_ManageStudents/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ManageStudents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Hello_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("We managed to do it");
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Lbx_Students.ItemsSource = App._students;
+            CoBx_gender.ItemsSource = App._genders;
+            Cobx_Picked.ItemsSource = new List<bool> { true, false };
+        }
+
+        private void Tbx_filter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var filter = (sender as TextBox).Text.ToLower();
+            var lst = from s in App._students where s.name.ToLower().Contains(filter) select s;
+            Lbx_Students.ItemsSource = lst;
+
+        }
+
+        private void Btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            Student stud = new Student { firstName = "Edit...", lastName = "Edit...", matNumber = Math.Abs(Guid.NewGuid().GetHashCode()).ToString()};
+
+
+            App._students.Add(stud);
+            Lbx_Students.SelectedItem = stud;
+            Lbx_Students.ScrollIntoView(stud);  // scrolled to the new student
+
+
+            //Workaround
+            //Lbx_Students.ItemsSource = null;
+            //Lbx_Students.ItemsSource =App._students
+
+            
+        }
+
+        private void Btn_Del_Click(object sender, RoutedEventArgs e)
+        {
+            var itm = Lbx_Students.SelectedItem;
+
+            if (itm == null)
+            {
+                MessageBox.Show("Please select an itm to be deleted first!!!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            var toDelete = itm as Student;
+
+            var res = MessageBox.Show($"Are you sure to delete {toDelete.firstName} {toDelete.lastName}?", "Error", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if(res == MessageBoxResult.OK)
+                App._students.Remove(toDelete);
+        }
+
+       
+
+        private void Btn_GoToWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new W_Test();
+            win.Owner = this;
+            Visibility = Visibility.Hidden;
+            win.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var lst = from s in App._students where s.gender == "f"  select s;
+            
+            //sorting
+            var lst = App._students.OrderBy(s =>s.firstName);
+            Lbx_Students.ItemsSource = lst;
+
+        }
+    }
+}
